0bc6108f148fab1ed6b7c41e03698d59164414b5
leanhkhoiii <leanhkhoi21703@gmail.com>
2024-10-01 10:38:08 +0700

    update security LAB 3

--- security/form_user.php
+++ security/form_user.php
@@ -4,58 +4,87 @@ session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
-$user = NULL; //Add new user
+$user = NULL; // Add new user
 $_id = NULL;
 
 if (!empty($_GET['id'])) {
     $_id = $_GET['id'];
-    $user = $userModel->findUserById($_id);//Update existing user
+    $user = $userModel->findUserById($_id); // Update existing user
 }
 
+$errors = [];
 
 if (!empty($_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $password = trim($_POST['password']);
 
-    if (!empty($_id)) {
-        $userModel->updateUser($_POST);
-    } else {
-        $userModel->insertUser($_POST);
+    if (empty($name)) {
+        $errors[] = "Bắt buộc nhập tên";
+    } elseif (!preg_match("/^[a-zA-Z0-9]{5,15}$/", $name)) {
+        $errors[] = "<b>Tên</b> phải là ký tự hợp lệ: A->Z, a->z, 0->9 <br> Chiều dài từ 5 đến 15 ký tự";
     }
-    header('location: list_users.php');
-}
 
+    if (empty($password)) {
+        $errors[] = "Bắt buộc nhập mật khẩu";
+    } elseif (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()])[a-zA-Z0-9~!@#$%^&*()]{5,10}$/", $password)) {
+        $errors[] = "<b>Mật khẩu</b> phải bao gồm: chữ thường (a->z), chữ HOA (A->Z), số (0-9) và ký tự đặc biệt: ~!@#$%^&*()
+        <br> Chiều dài từ 5 đến 10 ký tự";
+    }
+
+   
+    if (empty($errors)) {
+        if (!empty($_id)) {
+            $userModel->updateUser($_POST);
+        } else {
+            $userModel->insertUser($_POST);
+        }
+        header('location: list_users.php');
+        exit; 
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
-    <?php include 'views/header.php'?>
+    <?php include 'views/header.php' ?>
     <div class="container">
 
-            <?php if ($user || !isset($_id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
+        <?php if ($user || !isset($_id)) { ?>
+            <div class="alert alert-warning" role="alert">
+                User form
+            </div>
+            <?php
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }
+            ?>
+            <form method="POST">
+                <input type="hidden" name="id" value="<?php echo $_id ?>">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
                 </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $_id ?>">
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
-            <?php } ?>
+                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            </form>
+        <?php } else { ?>
+            <div class="alert alert-success" role="alert">
+                User not found!
+            </div>
+        <?php } ?>
     </div>
 </body>
+
 </html>
--- security/models/UserModel.php
+++ security/models/UserModel.php
@@ -2,17 +2,21 @@
 
 require_once 'BaseModel.php';
 
-class UserModel extends BaseModel {
+class UserModel extends BaseModel
+{
 
-    public function findUserById($id) {
-        $sql = 'SELECT * FROM users WHERE id = '.$id;
+    public function findUserById($id)
+    {
+        $decryptedId = $this->decryptId($id); 
+        $sql = 'SELECT * FROM users WHERE id = ' . $decryptedId;
         $user = $this->select($sql);
 
         return $user;
     }
 
-    public function findUser($keyword) {
-        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+    public function findUser($keyword)
+    {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %' . $keyword . '%' . ' OR user_email LIKE %' . $keyword . '%';
         $user = $this->select($sql);
 
         return $user;
@@ -24,9 +28,10 @@ class UserModel extends BaseModel {
      * @param $password
      * @return array
      */
-    public function auth($userName, $password) {
+    public function auth($userName, $password)
+    {
         $md5Password = md5($password);
-        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
+        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "' . $md5Password . '"';
 
         $user = $this->select($sql);
         return $user;
@@ -37,10 +42,11 @@ class UserModel extends BaseModel {
      * @param $id
      * @return mixed
      */
-    public function deleteUserById($id) {
-        $sql = 'DELETE FROM users WHERE id = '.$id;
+    public function deleteUserById($id)
+    {
+        $decryptedId = $this->decryptId($id); 
+        $sql = 'DELETE FROM users WHERE id = ' . $decryptedId;
         return $this->delete($sql);
-
     }
 
     /**
@@ -48,25 +54,41 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function updateUser($input) {
+    public function updateUser($input)
+    {    
+        $decryptedId = $this->decryptId($input['id']); 
         $sql = 'UPDATE users SET 
-                 name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) .'", 
-                 password="'. md5($input['password']) .'"
-                WHERE id = ' . $input['id'];
+             name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) . '", 
+             password="' . md5($input['password']) . '"
+            WHERE id = ' . $decryptedId;
 
         $user = $this->update($sql);
 
         return $user;
     }
 
+    // Mã hóa ID
+    public function encryptID($id)
+    {
+        return base64_encode($id);
+    }
+
+    // Giải mã ID
+    public function decryptID($encryptedId)
+    {
+        return base64_decode($encryptedId);
+    }
+
+
     /**
      * Insert user
      * @param $input
      * @return mixed
      */
-    public function insertUser($input) {
+    public function insertUser($input)
+    {
         $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".md5($input['password'])."')";
+            "'" . $input['name'] . "', '" . md5($input['password']) . "')";
 
         $user = $this->insert($sql);
 
@@ -78,10 +100,11 @@ class UserModel extends BaseModel {
      * @param array $params
      * @return array
      */
-    public function getUsers($params = []) {
+    public function getUsers($params = [])
+    {
         //Keyword
         if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] .'%"';
+            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] . '%"';
 
             //Keep this line to use Sql Injection
             //Don't change
